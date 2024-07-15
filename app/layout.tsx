35c16d662032108c933c05e06f59e1a1aa4ffec4
Rohit <rohitparih@gmail.com>
2024-07-15 18:02:40 +0530

    enhancement/seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {Head} from "next/document";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
