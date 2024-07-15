cadb7d75f84dd4cb1b7a17cf7ea28fd114b3addc
Rohit <rohitparih@gmail.com>
2024-07-15 17:24:00 +0530

    enhancement/seo

---
+++ app/sitemap.ts
@@ -0,0 +1,9 @@
+import {MetadataRoute} from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    return [
+        {
+            url: 'https://rohit.bloggios.com'
+        }
+    ]
+}
